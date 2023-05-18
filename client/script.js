195f757517a4b455a6a1a2b757e5f275ef2d130d
Prashanth P <test@test.com>
2023-05-18 18:15:23 +0530

    Initial Commit

---
+++ client/script.js
@@ -0,0 +1,100 @@
+import bot from './assets/bot.svg'
+import user from './assets/user.svg'
+
+const form = document.querySelector('form');
+const chatContainer = document.querySelector('#chat_container');
+
+let loadInterval;
+
+function loader(element) {
+  element.textContent = '';
+  loadInterval = setInterval(() => {
+    element.textContent += '.';
+    if (element.textContent === '....') {
+      element.textContent = '';
+    }
+  }, 300);
+}
+
+function typeText(element, text) {
+  let index = 0;
+  let interval = setInterval(() => {
+    if (index < text.length) {
+      element.textContent += text.charAt(index);
+      index++;
+    } else {
+      clearInterval(interval);
+    }
+  }, 20);
+}
+
+function generateUniqueId() {
+  const timeStammp = new Date().getDate();
+  const random = Math.random();
+  const hexadecimalString = random.toString(16);
+  return `id-${timeStammp}-${hexadecimalString}`;
+}
+
+function chatStripe(isAi, value, uniqueId) {
+  return (
+    `
+      <div class="wrapper ${isAi && 'ai'}">
+        <div class="chat">
+          <div class="profile">
+            <img
+              src="${isAi ? bot : user}"
+              alt="${isAi ? 'bot' : 'user'}"
+            />
+          </div>
+          <div class="message" id="${uniqueId}">${value}</div>
+        </div>
+      </div>
+    `
+  )
+}
+
+const handleSubmit = async (event) => {
+  event.preventDefault();
+  const data = new FormData(form);
+
+  //user's chat stripe
+  chatContainer.innerHTML += chatStripe(false, data.get('prompt'));
+
+  form.reset();
+
+  //bot's chat stripe
+  const uniqueId = generateUniqueId();
+  chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+  const messageDiv = document.getElementById(uniqueId);
+  loader(messageDiv);
+  //fetch data from the server -> bot's response
+  const response = await fetch('http://localhost:3000', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ prompt: data.get('prompt') }),
+  });
+  clearInterval(loadInterval);
+  messageDiv.innerHTML = '';
+  if(response.ok) {
+    const data = await response.json();
+    console.log(data);
+    const parsedData = data.message.trim();
+    typeText(messageDiv, parsedData);
+  } else {
+    const err = await response.text();
+    messageDiv.innerHTML = 'Sorry, something went wrong. Please try again later.';
+    alert(err);
+  }
+};
+
+form.addEventListener('submit', handleSubmit);
+form.addEventListener('keyup', (event) => {
+  if (event.keyCode === 13) {
+    handleSubmit(event);
+  }
+});
+
+//43:02
